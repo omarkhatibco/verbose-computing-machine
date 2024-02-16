47b392b9c34e54b515e93c28e7a728f05fcb5af1
Omar Khatib <omar.khatib@tourlane.com>
2024-02-16 09:48:41 +0100

    feat: basic design

---
+++ prettier.config.cjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^(feature/(.*)$)|^(feature-$)',
+    '^(ui/(.*)$)|^(ui-$)',
+    '^(data/(.*)$)|^(data-$)',
+    '^(util/(.*)$)|^(util-$)',
+    '',
+    // '^@/types(.*)$',
+    // '^@/components(.*)$',
+    // '^@/context(.*)$',
+    // '^@/hooks(.*)$',
+    // '^@/util(.*)$',
+    // '^@/libs(.*)$',
+    // '^@/styles(.*)$',
+    '',
+    '^[./]',
+    '^[../]',
+    '',
+  ],
+}
